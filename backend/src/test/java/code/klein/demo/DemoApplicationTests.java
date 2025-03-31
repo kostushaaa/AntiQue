2fca8cc488387c5538ae2e665990af9b3e72faaf
Kostiantyn <kostiantyncheshykhin@gmail.com>
2025-03-31 11:51:22 +0200

    init commit, PSQL, Demo RestController

---
+++ backend/src/test/java/code/klein/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package code.klein.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
